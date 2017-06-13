bd1fd1bb59788e5f9350ada1c547764d62dc0796
MBarkovskaya <mariya.barkovskaya@gmail.com>
2017-06-13 09:49:55 +0300

    задание №13

--- java-example/src/test/java/my_stqa/training/selenium/appmanager/SeleniumHelper.java
+++ java-example/src/test/java/my_stqa/training/selenium/appmanager/SeleniumHelper.java
@@ -7,6 +7,7 @@ import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.support.ui.WebDriverWait;
 import static org.openqa.selenium.support.ui.ExpectedConditions.presenceOfElementLocated;
+import static org.openqa.selenium.support.ui.ExpectedConditions.visibilityOf;
 
 public class SeleniumHelper {
 
@@ -43,6 +44,7 @@ public class SeleniumHelper {
       try {
         WebElement element = wait.until((WebDriver d) -> d.findElement(locator));
         if (element != null) {
+          wait.until(visibilityOf(element));
           element.click();
           clicked = true;
           break;
@@ -72,11 +74,15 @@ public class SeleniumHelper {
   }
 
   public boolean isElementPresent(By locator) {
+    return isElementPresent(locator, 10);
+  }
+
+  public boolean isElementPresent(By locator, int timeoutSecs) {
     //если элемент есть, то он найдется, если нет - будет выброшено исключение
     //так как Selenium не может знать есть элемент на странице или нет, поэтому нужно обернуть попытку поиска элементов в блок try
     try {
       //      явное ожидание
-      WebDriverWait wait = new WebDriverWait(driver, 10);
+      WebDriverWait wait = new WebDriverWait(driver, timeoutSecs);
       wait.until(presenceOfElementLocated(locator));
       //      неявное ожидание
       //      driver.findElement(locator);
--- java-example/src/test/java/my_stqa/training/selenium/tests/CartAddRemoveItemTest.java
+++ java-example/src/test/java/my_stqa/training/selenium/tests/CartAddRemoveItemTest.java
@@ -3,20 +3,18 @@ package my_stqa.training.selenium.tests;
 import org.junit.Test;
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebElement;
-import org.openqa.selenium.support.ui.ExpectedCondition;
+import org.openqa.selenium.support.ui.Select;
 import org.openqa.selenium.support.ui.WebDriverWait;
 
 import java.util.HashSet;
-import java.util.List;
 import java.util.Set;
 
-import static org.openqa.selenium.support.ui.ExpectedConditions.numberOfElementsToBe;
-import static org.openqa.selenium.support.ui.ExpectedConditions.textToBePresentInElementLocated;
+import static org.openqa.selenium.support.ui.ExpectedConditions.*;
 
 public class CartAddRemoveItemTest extends TestBase {
 
   @Test
-  public void CartAddRemoveItem () {
+  public void CartAddRemoveItem() {
     init();
     driver.get(getProperty("web.baseUrl") + "en/");
 
@@ -29,17 +27,22 @@ public class CartAddRemoveItemTest extends TestBase {
     driver.findElement(By.cssSelector("div#cart a.link")).click();
     int expectedItemsCount = itemNames.size();
     while (expectedItemsCount > 0) {
-// метод click()  в классе SeleniumHelper
       selenium().click(By.cssSelector("ul.items button[name=remove_cart_item]"), 10, 1000);
-      wait.until(numberOfElementsToBe(By.cssSelector("tr td.item"), expectedItemsCount --));
+      wait.until(numberOfElementsToBe(By.cssSelector("tr td.item"), --expectedItemsCount));
     }
   }
-  
+
 
   public String addItemToCard(WebDriverWait wait, String expectedText) {
+    wait.until(presenceOfElementLocated(By.cssSelector("div#box-most-popular a.link")));
     String text = driver.findElement(By.cssSelector("div#box-most-popular a.link")).getAttribute("title");
     driver.findElement(By.cssSelector("div#box-most-popular a.link")).click();
-    driver.findElement(By.cssSelector("div.buy_now button[name=add_cart_product]")).click();
+    if (selenium().isElementPresent(By.cssSelector("div.buy_now td.options"), 1)) {
+      WebElement el = driver.findElement(By.cssSelector("select[name='options[Size]']"));
+      Select select = new Select(el);
+      select.selectByVisibleText("Small");
+    }
+    selenium().click(By.cssSelector("div.buy_now button[name=add_cart_product]"), 10, 1000);
     wait.until(textToBePresentInElementLocated((By.cssSelector("div#cart span.quantity")), expectedText));
     driver.findElement(By.cssSelector("i.fa.fa-home")).click();
     return text;
